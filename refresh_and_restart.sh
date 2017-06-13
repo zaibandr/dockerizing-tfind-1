b7d3a6baa3f78d64e8603e2a4b2382ca2db2d6fd
panagoa <panagoa@mail.ru>
2017-06-13 17:14:02 +0300

    Create refresh_and_restart.sh

--- refresh_and_restart.sh
+++ refresh_and_restart.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-git pull https://github.com/zaibandr/dockerizing-tfind
+git pull https://github.com/panagoa/dockerizing-tfind
 docker-compose down
 docker volume rm webdata_app
 docker-compose build
